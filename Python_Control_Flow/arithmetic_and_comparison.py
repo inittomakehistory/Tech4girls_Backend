d79b2cfd0e41f33cfcafa6e5b0c583404e4d496a
inittomakehistory <victoriakissiedu5@gmail.com>
2024-12-14 21:02:50 +0000

    Create arithmetic_and_comparison.py
    arithmetic_and_comparison.py.

---
+++ Python_Control_Flow/arithmetic_and_comparison.py
@@ -0,0 +1,24 @@
+# python basics 
+# a script that defines two variables, x and y, with any values
+# Use +, -, *, /, //, **, and % to perform calculations between x and y
+# Compare x and y using ==, !=, <, >, <=, and >=.
+# Print the result of each calculation and comparison using f-strings
+
+x = 3
+y = 10
+
+print(f" x + y = { x + y}")
+print(f" x - y = { x - y}")
+print(f" x * y = { x * y}")
+print(f" x / y = { x / y}")
+print(f" x // y = { x // y}")
+print(f" x ** y = { x ** y}")
+print(f" x % y = { x % y}")
+
+#comparison operations 
+print(f" x == y = {x == y}")
+print(f" x != y = {x != y}")
+print(f" x < y = {x < y}")
+print(f" x > y = {x > y}")
+print(f" x <= y = {x <= y}")
+print(f" x >= y = {x >= y}")
