3160ad820900ff94971dfac7dd66f1cec3e8d521
inittomakehistory <victoriakissiedu5@gmail.com>
2024-12-14 20:35:27 +0000

    Create control_flow.py
    control_flow.py

---
+++ Python_Control_Flow/control_flow.py
@@ -0,0 +1,10 @@
+# python basics 
+# a script that asks the user for their age and:
+
+age = int(input("enter your age: "))
+if age < 18     
+  print("You are a minor")
+elif 18 <= age < 65:
+  print("You are an adult")
+else:
+ print("You are a senior")
