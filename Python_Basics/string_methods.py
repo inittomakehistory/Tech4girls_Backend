bd15be605e1fe89664c13ae4e1939e82900d6322
inittomakehistory <victoriakissiedu5@gmail.com>
2024-12-14 16:14:08 +0000

    Create string_methods.py
    string_methods.py

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,17 @@
+# python3 
+# upper()
+mv_string = "Hello,world"
+print("Upper case:",mv_string.upper())
+
+# lower()
+mv_string = "HELLO,WORLD"
+print("Lowercase:",mv_string.lower())
+
+# replace()
+mv_string = "Hello,world,you are beautiful"
+new_word = mv_string.replace("beautiful","big")
+print("Replaced:",new_word)
+
+# join()
+mv_string = ["hello", "world"]
+print("Join:","".join(mv_string))
