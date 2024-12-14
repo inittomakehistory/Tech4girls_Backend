c2e4434b00c3bd0e6dc56a9c15f7a4ae6cec1916
inittomakehistory <victoriakissiedu5@gmail.com>
2024-12-14 17:44:44 +0000

    Create list_and_set_methods.py
    list_and_set_methods.py

---
+++ Python_Control_Flow/list_and_set_methods.py
@@ -0,0 +1,55 @@
+# python basics 
+# append()
+example 
+lists = [1, 2, 3, 4, 5]
+lists.append("6")
+print(lists)
+
+# remove()
+lists = [1, 2, 3, 4, 5]
+lists.remove(4)
+print(lists)
+
+# pop()
+lists = [1, 2, 3, 4, 5]
+lists.pop(4)
+print(lists)
+
+# sort ()
+lists = [1, 2, 3, 4, 5]
+lists.sort()
+print(lists)
+
+# reverse ()
+lists = [1, 2, 3, 4, 5]
+lists.reverse()
+print(lists)
+
+# add()
+lists = [1, 2, 3, 4, 5]
+lists.insert(4,"four")
+print(lists)
+
+# remove()
+lists = [1, 2, 3, 4, 5]
+lists.remove(3)
+print(lists)
+
+# union()
+setA = {1, 2, 3}
+setB = {4, 5, 6}
+union_set = setA | setB
+print(union_set)
+
+# intersection()
+setA =  {1, 2, 3}
+setB = {4, 5, 6}
+intersection_ set = setA & setB 
+print(intersection_set)
+
+# difference()
+setA =  {1, 2, 3}
+setB = {4, 5, 6}  
+difference_set =  setA - setB 
+print(difference_set)
+
