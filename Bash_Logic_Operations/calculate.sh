09ea51ce75c2d41d1827b8d6dd9ec107bc6bfd41
inittomakehistory <victoriakissiedu5@gmail.com>
2024-12-14 14:46:51 +0000

    Create calculate.sh
    calculate.sh

---
+++ Bash_Logic_Operations/calculate.sh
@@ -0,0 +1,8 @@
+Number1=$1
+Number2=$2
+Number3=$3
+sum=$((Number1 + Number2))
+product=$((sum * Number3))
+echo " The sum of $Number1 and Number2 is $sum"
+echo "multiplying the sum by $Number3 gives:$product"
+fi
