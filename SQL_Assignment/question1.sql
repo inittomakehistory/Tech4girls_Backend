2546c10271226965d5bb7b55016a7c84b23ad117
inittomakehistory <victoriakissiedu5@gmail.com>
2024-12-16 14:32:02 +0000

    Update question1.sql

--- SQL_Assignment/question1.sql
+++ SQL_Assignment/question1.sql
@@ -1,10 +1,11 @@
--- creating a database
+-- creating a database named Tech4Girls_DB
 CREATE DATABASE IF NOT EXISTS Tech4Girls_DB;
 SHOW DATABASES;
 
-
+-- Use the Tech4Girls_DB database
 USE Tech4Girls_DB;
 
+-- Create a table Users
 CREATE TABLE IF NOT EXISTS Users( 
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
@@ -15,11 +16,13 @@ CREATE TABLE IF NOT EXISTS Users(
 
 SHOW TABLES;
 
-
+-- Insert sample data into the Users table
 INSERT INTO Users (username, email, created_at)
 VALUES ('ama','ama@example.com','2024-11-01 10:30:00'),
 ('Abena','abena@example.com','2024-11-02 12:00:00'),
 ('adjoa','adjoa@example.com','2024-11-03 14:15:00');
 
-SELECT * FROM Users;
+SELECT * FROM Users
+  
+
 
